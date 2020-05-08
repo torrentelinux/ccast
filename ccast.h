ede8ffba1293fc691ee44e15da22ab827b01dc6f
Torrentarium <torrentelinux@gmail.com>
2020-05-07 21:07:15 -0300

    Add files via upload

--- ccast.h
+++ ccast.h
@@ -5,7 +5,7 @@
 //		   Traducir el lenguaje C/C++ al lenguaje Castellano.
 //		   Traducción al Castellano de las palabras reservadas
 //		   de C y de C++ del compilador Embarcadero C++ Builder.
-// Versión       : v1.3 - Marzo de 2019.
+// Versión       : v1.5 - Mayo de 2020.
 // Licencia      : Copyright (c) 2019-2020 Octulio Biletán - torrentelinux@gmail.com
 //		   This file, ccast.h, is licensed under the GNU General Public License v3.0
 //
@@ -13,60 +13,87 @@
 #if !defined(__CCAST__)
 #   define __CCAST__		/* constante de CCAST.H */
 
-#define deftipo			typedef
 #define automático		auto
-#define externo			extern
-#define registro		register
-#define volátil			volatile
-#define estático		static
-#define constante		const
-#define lógico			bool
 #define caracter		char
 #define caracter_amplio		wchar_t
+#define constante		const
+#define con_signo		signed
 #define corto			short
+#define deftipo			typedef
+#define doble			double
 #define entero			int
 #define entero_corto		short int
 #define entero_doble		long int
+#define estático		static
+#define externo			extern
+#define flotante		float
 #define largo			long
 #define largo_doble		long long
-#define flotante		float
-#define doble			double
-#define con_signo		signed
-#define sin_signo		unsigned
+#define _lógico			_Bool
+#define lógico			bool
 #define nada			void
+#define registro		register
+#define sin_signo		unsigned
+#define volátil			volatile
 
-#define unión			union
-#define estructura		struct
+#define _complejo		_Complex
+#define _imaginario		_Imaginary
+
+/* Caso especial: los números naturales, incluído cero.
+   No se trata de una traducción inglés-castellano sino
+   de una clasificación por tipos de datos entero, desde
+   cero hasta el máximo permitido por la arquitectura del
+   microprocesador de la computadora.
+*/
+#define natural			unsigned int
+#define natural_corto		unsigned short int
+#define natural_grande		unsigned long int
+#define natural_inmenso		unsigned long long
+#define natural_enorme		unsigned __int64
+
+/* Caso especial: el conjunto de caracteres ASCII (7 bits)
+   y el conjunto de caracteres ASCII extendido (8 bits).
+   Se contempla la codificación de caracteres ASCII 
+   tanto del conjunto de códigos entre -127 y +128 y del
+   conjunto de códigos comprendidos entre 0 y 255.
+*/
+#define ascii			signed char
+#define ascii_extendido		unsigned char
+
 #define enumeración		enum
+#define estructura		struct
+#define unión			union
 
-#define retornar		return
+#define caso			case
+#define conmutar		switch
+#define continuar		continue
+#define dejar			break
+#define hacer			do
+#define ir_a			goto
 #define mientras		while
 #define para			for
-#define hacer			do
-#define continuar		continue
+#define por_omisión		default
+#define retornar		return
 #define si			if
 #define sino			else
-#define dejar			break
-#define conmutar		switch
-#define caso			case
-#define por_omisión		default
-#define ir_a			goto
 
 #define __intentar		__try
-#define __excepción		__except
 #define __enlínea		__inline
+#define enlínea			inline
+#define __excepción		__except
 
-#define puntero_genérico		void*
 #define puntero_caracteres		char*
 #define puntero_caracteres_amplio	wchar_t*
+#define puntero_genérico		void*
+#define restringir			restrict
 
-#define verdadero		true
 #define falso			false
+#define verdadero		true
 
 #define ensam			asm
-#define tamde			sizeof
+#define tam_de			sizeof
 
-// Palabras reservadas de C++: listado incompleto...
+// Palabras reservadas de C++:
 #ifdef __cplusplus
 #  define clase			class
 #  define amiga			friend
@@ -84,9 +111,21 @@
 #  define operador		operator
 #  define _y_			and
 #  define _o_			or
+#  define o_excluyente		xor
+#  define o_excluyente_igual	xor_eq
 #  define _no_			not
-#  define idtipo		typeid
-#  define decltipo		decltype
+#  define id_tipo		typeid
+
+//#  define escribe_nombre	typename
+#  define nombre_tipo		typename
+
+#  define decl_tipo		decltype
+#  define tipo_de		typeof
+#  define aparente		virtual
+#  define hebra			__thread
+#  define alineado_a		alignof
+#  define uuid_de		uuidof
+#  define __uuid_de		__uuidof
 #endif
 // -----------------------------------
 
@@ -103,6 +142,18 @@
 // número precisión doble  --> double
 #define número
 #define precisión
+
+// Alias de restringir
+#define limitar			restringir
+
+// Alias de hebra
+#define hilo			hebra
+
+// Alias de o_excluyente
+#  define _o_ex			o_excluyente
+
+// Alias de __excepción
+#define exceptuar		__excepción
 
 // Alias de 'para'
 #define contar			para
